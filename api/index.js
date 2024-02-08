5e1ffdb45b0987dcc4f2b66b8713e464f7d8a231
HanaZadravec <75135070+HanaZadravec@users.noreply.github.com>
2024-02-08 12:20:10 +0100

    login and registration done

--- api/index.js
+++ api/index.js
@@ -11,6 +11,7 @@ dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL);
 const jwtSecret = process.env.JWT_SECRET;
+bcryptSalt= bcrypt.genSaltSync(10);
 
 mongoose.connection.on('connected', () => {
     console.log('Database connected');
@@ -20,21 +21,36 @@ const app = express();
 app.use(express.json());
 const port = 5000;
 
+
 app.use(cookieParser());
-console.log(process.env.CLIENT_URL);
 app.use(cors({
   credentials: true,
-  origin: 'http://localhost:5173',
+  origin: process.env.CLIENT_URL,
 }));
 
 app.get('/', (req, res) => {
     res.json('test ok');
 });
 
+app.get('/profile', (req,res) => {
+  const {token} = req.cookies;
+  if(token){
+    jwt.verify(token, jwtSecret, {}, (err, user) => {
+      if(err) {
+        return res.status(401).json('not authorized');
+      }
+      res.json(user);
+    }); 
+  }else{
+    res.status(401).json('not authorized');
+  }
+  
+});
+
 app.post('/register', async (req,res) => {
     const {username,password} = req.body;
     try {
-      const hashedPassword = bcrypt.hashSync(password,10);
+      const hashedPassword = bcrypt.hashSync(password,bcryptSalt);
       const createdUser = await User.create({
         username:username,
         password:hashedPassword,
@@ -48,6 +64,22 @@ app.post('/register', async (req,res) => {
       res.status(500).json('error');
     }
   });
+
+app.post('/login', async (req,res) => {
+  const {username,password} = req.body;
+  const foundUser=await User.findOne({username});
+  if(foundUser){
+    if(bcrypt.compareSync(password,foundUser.password)){
+      jwt.sign({userId:foundUser._id,username}, jwtSecret, {}, (err, token) => {
+        res.cookie('token', token, {sameSite:'none', secure:true}).status(200).json({
+          id: foundUser._id,
+        });
+      });
+    }else{
+      res.status(401).json('not authorized');
+  }
+}});
+
 
 app.listen(port, () => {
     console.log(`App is running on ${port}`);
